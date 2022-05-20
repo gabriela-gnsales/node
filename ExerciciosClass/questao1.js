92fea2289dd60ffe39066169f8d064edf166f34b
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-05-20 16:55:06 -0300

    Questão 1 Class

--- ExerciciosClass/questao1.js
+++ ExerciciosClass/questao1.js
@@ -10,30 +10,29 @@ Implemente a requisição utilizando Promise e async/await conforme vimos até e
 
 // Importar o módulo fetch
 
+// import fetch from 'node-fetch';
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 // Promise
 
-// fetch('https://pokeapi.co/api/v2/pokemon/?limit=151', { method: 'GET' })
-//     // .then(resposta => resposta.text())
-//     .then(resposta => resposta.json())
-//     .then(texto => console.log(texto))
-//     .catch(error => console.log(error));
+fetch('https://pokeapi.co/api/v2/pokemon/?limit=151', { method: 'GET' })
+// fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    // .then(response => response.text())
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
 
 // async/await
 
-
-// async function getPokeAPI() {
-//     try {
-//         await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
-//         const res = await resposta.json();
-//         return console.log(res);
-//     } catch (error) {
-//         console.log(error);
-//     }
-    
-// }
-
-
-
-
+async function getPokeAPI() {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }   
+}
+getPokeAPI().then(data => {
+    console.log(data);
+  });
